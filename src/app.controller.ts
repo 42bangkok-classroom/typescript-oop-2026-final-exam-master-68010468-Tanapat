8cdb117432b2cb33562202fb6e2b29c24c8a7b71
68010468-Tanapat <68010468@kmitl.ac.th>
2026-03-25 11:03:22 +0700

    3

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
+  getHello() {
+  const result = this.appService.getHello();
+  return {
+    succes: true,
+    data: result,
+    message: 'Hello NestJS'
+};
+}
 }
